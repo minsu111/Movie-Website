acd88a65cb0089d2770666ad91bb7ecf6094b10a
[SW5] 이수민 <tnklr1111@gmail.com>
2023-06-03 04:38:42 +0900

    movie website_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch("https://api.themoviedb.org/3/movie/popular?api_key=a960ad448a61fb42f64ef81e2c1055ad"
+      )
+    ).json();
+    setMovies(json.results);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return <div>{loading ? <h1>Loading...</h1> : <div>{movies.map(movie => <div key={movie.id}>
+    <img src={getImageUrl(movie.poster_path)} />
+    <h2>{movie.title}</h2>
+    <p>opening date: {movie.release_date} </p>
+    <p>{movie.overview}</p>
+  </div>
+  )}
+  </div>
+  }
+  </div>;
 }
 
 export default App;
+export const getImageUrl = (path, size = 200) => {
+  return `https://image.tmdb.org/t/p/w${size}${path}`;
+};
+
+/*
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch("https://api.themoviedb.org/3/movie/popular?api_key=a960ad448a61fb42f64ef81e2c1055ad"
+      )
+    ).json();
+    setMovies(json.results);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : 
+        <div>
+          {movies.map(movie => (
+            <div key={movies.id}>
+              <img src={getImageUrl(poster_path)} />
+              <h2>{movie.title}</h2>
+              <p>opening date: {release_date} </p>
+              <p>{overview}</p>
+            </div>
+          ))}}
+        </div>
+      )
+    </div>
+        
+*/
+
